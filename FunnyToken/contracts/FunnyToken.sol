7acc2a2f15802a2be43a45b0de8ec2e2e49fbc8a
Flávio Codeço Coelho <fccoelho@gmail.com>
2018-10-04 09:11:16 -0300

    Added initial contracts

---
+++ FunnyToken/contracts/FunnyToken.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.24;
+
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract FunnyToken is ERC20{
+
+}
+
---
+++ FunnyToken/migrations/2_deploy_contract.js
@@ -0,0 +1,4 @@
+var  FunnyToken = artifacts.require("FunnyToken");
+    module.exports  =  function(deployer) {
+        deployer.deploy(FunnyToken);
+};
