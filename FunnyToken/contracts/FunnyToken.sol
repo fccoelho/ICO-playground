31296e771f12d834f5a9262f5c6b945940ade7ed
Flávio Codeço Coelho <fccoelho@gmail.com>
2018-10-09 10:31:21 -0300

    IMplemented constructor for FunnyToken issue #1

--- FunnyToken/contracts/FunnyToken.sol
+++ FunnyToken/contracts/FunnyToken.sol
@@ -4,6 +4,13 @@ import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 
 contract FunnyToken is ERC20 {
+    uint256 private _totalSupply;
+    address private owner;
 
+    constructor(uint256 supply) public {
+        _totalSupply = supply;
+        owner = msg.sender;
+        _mint(owner, supply/10);
+    }
 }
 
