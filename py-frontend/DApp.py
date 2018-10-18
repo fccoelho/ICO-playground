ec996b17d8405554c2fb025d567a96307cf62678
Flávio Codeço Coelho <fccoelho@gmail.com>
2018-10-18 17:49:07 -0300

    Fixed migration script Contract addresses are saved in the artifacts after deployment. closes issue #15

--- py-frontend/DApp.py
+++ py-frontend/DApp.py
@@ -1,18 +1,28 @@
 from flask import Flask
 from flask import render_template
 from flask_foundation import Foundation
+from flask import request
 import web3
 from web3 import Web3, HTTPProvider, TestRPCProvider
 from web3.contract import ConciseContract
 import json
 
 
+
+
 w3 = Web3()  # Auto-detects connection settings
-contract_address = Web3.toChecksumAddress('0xc95dd12e7ddf931c0cf7b802394fb5b99e1bfe89')
+
 with open('../FunnyToken/build/contracts/FunnyToken.json') as f:
     token_artifact = json.load(f)
+with open('../FunnyToken/build/contracts/TokenSale.json') as f:
+    sale_artifact = json.load(f)
 
-token_contract_instance = w3.eth.contract(address=contract_address, abi=token_artifact['abi'])
+token_address = Web3.toChecksumAddress(list(token_artifact['networks'].values())[0]['address'])
+sale_address = Web3.toChecksumAddress(list(sale_artifact['networks'].values())[0]['address'])
+
+
+token_contract_instance = w3.eth.contract(address=token_address, abi=token_artifact['abi'])
+sale_contract_instance = w3.eth.contract(address=sale_address, abi=sale_artifact['abi'])
 account = w3.eth.accounts[0]
 
 app = Flask(__name__)
@@ -25,7 +35,14 @@ app.config['SECRET_KEY'] = 'devkey'
 
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    bal = token_contract_instance.functions.balanceOf(account).call()
-    return render_template('index.html', balance=bal, address=contract_address, account=account)
+    if request.method == 'POST':
+        try:
+            address = Web3.toChecksumAddress(request.form['address'])
+        except ValueError:
+            pass
+    else:
+        address = account
+    bal = token_contract_instance.functions.balanceOf(address).call()
+    return render_template('index.html', balance=bal, address=sale_address, account=account)
