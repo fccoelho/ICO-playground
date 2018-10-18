20615b13ed1011c0842e1da193fa79d80b93a158
Flávio Codeço Coelho <fccoelho@gmail.com>
2018-10-18 14:43:03 -0300

    Fixed migration script Contract addresses are saved in the artifacts after deployment. closes issue #15

--- FunnyToken/migrations/2_deploy_contract.js
+++ FunnyToken/migrations/2_deploy_contract.js
@@ -1,21 +1,9 @@
-const fs  = require('fs');
 var  FunnyToken = artifacts.require("FunnyToken");
 var  TokenSale = artifacts.require("TokenSale");
-    module.exports  = async function(deployer, network, accounts) {
-        deployer.deploy(FunnyToken, 1000000000000)
-            .then(function(){
-                deployer.deploy(TokenSale, 1, accounts[0], FunnyToken.address);
-            });
-
-        var T = FunnyToken.deployed();
-        var S = TokenSale.deployed();
-        const storage = {'TOKEN_ADDRESS': T.address,
-                        'SALE_ADDRESS': S.address
-        };
-        const output = JSON.stringify(storage);
-        console.log(output)
-        fs.writeFileSync('.address.json',output, 'utf-8');
-
-
-        // console.log(process.env.TOKEN_ADDRESS)
+var storage = {};
+module.exports  = function(deployer, network, accounts) {
+    return deployer.deploy(FunnyToken, 1000000000000)
+        .then(()=>{
+            return deployer.deploy(TokenSale, 10, accounts[0], FunnyToken.address)
+        });
 };
