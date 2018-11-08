05037c263d6ec559b3f224191cd48338be5d65e4
Flávio Codeço Coelho <fccoelho@gmail.com>
2018-11-08 12:20:28 -0200

    Added configuration for deployment on Ropstein

--- FunnyToken/truffle.js
+++ FunnyToken/truffle.js
@@ -13,12 +13,23 @@
  * ```
  */
 
+var HDWalletProvider = require("truffle-hdwallet-provider")
+const mnemonic = "beyond eager yard box upper orbit because urban youth mystery render miracle"
+//beyond eager yard box upper orbit because urban youth mystery render miracle
+
 module.exports = {
  networks : {
     ganache : {
       host : 'localhost',
       port : 8545,    // By default Ganache runs on this port.
       network_id : "*" // network_id for ganache is 5777. However, by keeping * as value you can run this node on  any network
-    }
+    },
+     ropsten: {
+        provider: function(){
+            return new HDWalletProvider(mnemonic,'https://ropsten.infura.io/v3/8f62d68e09944a559e788e8f73a7f4ed')
+        },
+         network_id: 3,
+         gas: 4500000,
+     }
   }
 };
